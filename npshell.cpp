af342083ec266a51fb64c17aedf1185e10ed35c5
Chia-Yu, Sun <chiayusun.cs08g@nctu.edu.tw>
2019-10-15 01:08:21 +0800

    1. add exec_cmd draft(fork) 2. modify cmd parse to stringstream (fix single cmd bug) 3. add exit

--- npshell.cpp
+++ npshell.cpp
@@ -4,9 +4,11 @@
 #include <string.h>
 #include <algorithm>
 #include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define PROMPT_SYMBOL "%" 
-enum Status{NO_ERROR, EXIT, ERROR};
+enum Status{SUCCESS, EXIT, ERROR};
 
 typedef struct {
     std::string cmd;
@@ -90,30 +92,66 @@ std::vector<Command> parse_cmds(std::string usr_ipt){
     }
 
     // split cmd by space
-    char cmd_str[256];
-    strcpy(cmd_str, c.c_str());
-    char* ptr_s = strtok(cmd_str, " ");
-
+    std::stringstream cmd_stream;
+    cmd_stream.str(c);
+    
     // frist word (cmd)
-    cmd.cmd = std::string(ptr_s);
-    while (cmd.cmd.find(' ') != std::string::npos){
-        cmd.cmd.erase(std::remove(cmd.cmd.begin(), cmd.cmd.end(), ' '), cmd.cmd.end());  
-    }
+    cmd_stream >> cmd.cmd;
+
     // second word (input file)
-    ptr_s = strtok(ptr_s+strlen(ptr_s)+1, " ");
-    if (ptr_s != NULL && ptr_s[0]!='>'){
-      cmd.in_file = std::string(ptr_s);
-      while (cmd.in_file.find(' ') != std::string::npos){
-          cmd.in_file.erase(std::remove(cmd.in_file.begin(), cmd.in_file.end(), ' '), cmd.in_file.end());  
-      }
+    std::string i_file;
+    cmd_stream >> i_file;
+    
+    if (!cmd_stream.fail()){
+      cmd.in_file = i_file;
     }
+    
+    cmds.push_back(cmd);
+
+    // if no any pipe symbol
+    if (usr_ipt.find('|') == std::string::npos)
+      break;
 
     ptr = strtok(ptr+strlen(ptr)+1, "|");
-    cmds.push_back(cmd);
   }
 
   return cmds;
 }
+
+int exec_cmd(Command cmd){
+  int status;
+  pid_t pid;
+
+  // check if exit
+  if (cmd.cmd == "exit"){
+    return EXIT;
+  }
+  
+
+  // fork a child to exec the cmd
+  pid = fork();
+
+  switch (pid){
+  case -1:
+    std::cerr << "fork error" << std::endl;
+    break;
+
+  case 0: // child
+    std::cout << "I'm child process, exec " << cmd.cmd << std::endl;
+    exit(0);
+    break;
+  
+  default: // pid > 0, parent
+    std::cout << "I'm parent, wait for child" << std::endl;
+    wait(&status);
+    std::cout << "catch child, status=" << status << std::endl;
+    status = SUCCESS;
+    break;
+  }
+
+  return status;
+}
+
 
 int get_cmd(){
   int status = 0;
@@ -127,26 +165,23 @@ int get_cmd(){
   // parse
   cmds = parse_cmds(usr_input);
 
-  for (size_t i = 0; i < cmds.size(); i++){
-    std::cout << cmds[i] << std::endl;
-  }
   // exec
+  for (size_t i = 0; i < cmds.size(); i++){
+    status = exec_cmd(cmds[i]);
+  }
   // return exec status
 
   return status;
 }
 
-int exec_cmd(){
-  int status;
-  return status;
-}
+
 
 
 int main(){
   int status;
   do{
     status = get_cmd();
-  } while (status == NO_ERROR);
+  } while (status == SUCCESS);
   
   return 0;
 }
