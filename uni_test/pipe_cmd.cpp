f49b26186b4857b45206b91c11632bc410995b3d
cysun <chiayusun.cs08g@nctu.edu.tw>
2019-10-16 18:24:12 +0800

    a success pipe sample code

--- uni_test/pipe_cmd.cpp
+++ uni_test/pipe_cmd.cpp
@@ -15,72 +15,95 @@ int main() {
     // cat test.html |2 removetag test.html | number
     std::vector<Command> cmds;
 
-    
+    int fd[2][2];
 
-    int fd[8];
     pid_t pid;
     char buf[1024];
-    pipe(fd);
-
+    for (size_t i = 0; i < 2; i++){
+        pipe(fd[i]);
+    }
+    
     pid = fork();
 
     // child1
-    int pipe_id = 0;
     if (pid == 0){
-        // close(fd[pipe_id*2+WRITE]);
-        close(fd[pipe_id*2+READ]);
-        dup2(fd[pipe_id*2+WRITE], STDOUT_FILENO); // dup stdout to pipe_id
+        
+        dup2(fd[0][WRITE], STDOUT_FILENO); // dup stdout to pipe_id
+
+        for (size_t i = 0; i < 2; i++){
+            close(fd[i][READ]);
+            close(fd[i][WRITE]);
+        }  
         
         execlp("bin/cat", "bin/cat", "test.html", (char*) NULL);
         std::cerr << "execute failed" << std::endl;
         exit(1);
     }
-    
-    int status;
-    waitpid(pid, &status, 0); 
-    // waitpid(pid, &status, WNOHANG); 
-    // WNOHANG: return immediately if no child has exited. 
-    //          to handle child first
 
-    std::cout << "parent catch child 1" << std::endl;
+    int status;
 
     // child2
     pid_t pid2;
     pid2 = fork();
     if (pid2 == 0){
-        // dup2(fd[READ], STDIN_FILENO); // dup stdin to in
-        // dup2(fd[WRITE], STDOUT_FILENO); // dup output to stdout
-        // close(fd[WRITE]);
-        // close(fd[READ]);
-        execlp("bin/removetag", "bin/removetag", "test.html", (char*) NULL);
-        std::cerr << "execute failed" << std::endl;
-        exit(1);
-    }
+        dup2(fd[0][READ], STDIN_FILENO); // dup output to stdout
+        dup2(fd[1][WRITE], STDOUT_FILENO); // dup stdin to in
 
-        
-    waitpid(pid2, &status, 0); 
-    // waitpid(pid2, &status, WNOHANG);
-    std::cout << "parent catch child 2" << std::endl;    
-
-    // child3
-    pid_t pid3;
-    pid3 = fork();
-    if (pid3 == 0){
-        waitpid(pid, &status, 0);
-        waitpid(pid2, &status, 0); 
-        close(fd[pipe_id*2+WRITE]);
-        dup2(fd[pipe_id*2+READ], STDIN_FILENO); // dup pipe of 1 to in
-        // dup2(fd[pipe_id*2+WRITE], STDOUT_FILENO);
-        // close(fd[pipe_id*2+READ]);
+        for (size_t i = 0; i < 2; i++){
+            close(fd[i][READ]);
+            close(fd[i][WRITE]);
+        }  
         
         execlp("bin/number", "bin/number", (char*) NULL);
         std::cerr << "execute failed" << std::endl;
         exit(1);
     }
-    close(fd[pipe_id*2+WRITE]);
-    waitpid(pid3, &status, 0); 
-    // waitpid(pid3, &status, WNOHANG);
-    std::cout << "parent catch child 3" << std::endl;    
+
+    // child3
+    pid_t pid3;
+    pid3 = fork();
+    if (pid3 == 0){
+        // waitpid(pid2, &status, 0); 
+        
+        dup2(fd[1][READ], STDIN_FILENO); // dup pipe of 1 to in
+
+        for (size_t i = 0; i < 2; i++){
+            close(fd[i][READ]);
+            close(fd[i][WRITE]);
+        }  
+        
+        // std::cout << "I'm child 3" << std::endl;
+        execlp("bin/ls", "bin/ls", "-l", (char*) NULL);
+        std::cerr << "execute failed" << std::endl;
+        exit(1);
+    }
+
+    // child4
+    pid_t pid4;
+    pid4 = fork();
+    if (pid4 == 0){       
+        dup2(fd[1][READ], STDIN_FILENO); // dup pipe of 1 to in
+
+        for (size_t i = 0; i < 2; i++){
+            close(fd[i][READ]);
+            close(fd[i][WRITE]);
+        }  
+        
+        // std::cout << "I'm child 3" << std::endl;
+        execlp("bin/number", "bin/number", (char*) NULL);
+        std::cerr << "execute failed" << std::endl;
+        exit(1);
+    }
+
+    for (size_t i = 0; i < 2; i++){
+            close(fd[i][READ]);
+            close(fd[i][WRITE]);
+    }  
+
+    for (size_t i = 0; i < 4; i++){
+        wait(&status);    
+        std::cout << "parent catch child" << std::endl;
+    }
     
     return 0;
 }
