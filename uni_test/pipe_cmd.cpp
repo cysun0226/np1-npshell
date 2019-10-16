e684dcc1b50d375f1a2958853d5f89ab267784ab
cysun <chiayusun.cs08g@nctu.edu.tw>
2019-10-16 16:52:45 +0800

    fix uni_test/pipe_cmd (pipe can only use 0&1?)

--- uni_test/pipe_cmd.cpp
+++ uni_test/pipe_cmd.cpp
@@ -5,7 +5,7 @@
 #include <wait.h>
 #include <iostream>
 #include "npshell.h"
-
+#include <vector>
 
 
 
@@ -13,6 +13,9 @@
 int main() {
     /* sample input */
     // cat test.html |2 removetag test.html | number
+    std::vector<Command> cmds;
+
+    
 
     int fd[8];
     pid_t pid;
@@ -22,13 +25,11 @@ int main() {
     pid = fork();
 
     // child1
-    // pipe_id = 1;
+    int pipe_id = 0;
     if (pid == 0){
-        int pipe_id = 0;
         // close(fd[pipe_id*2+WRITE]);
         close(fd[pipe_id*2+READ]);
         dup2(fd[pipe_id*2+WRITE], STDOUT_FILENO); // dup stdout to pipe_id
-        
         
         execlp("bin/cat", "bin/cat", "test.html", (char*) NULL);
         std::cerr << "execute failed" << std::endl;
@@ -60,22 +61,23 @@ int main() {
     waitpid(pid2, &status, 0); 
     // waitpid(pid2, &status, WNOHANG);
     std::cout << "parent catch child 2" << std::endl;    
-    std::cout.flush();
 
     // child3
     pid_t pid3;
     pid3 = fork();
     if (pid3 == 0){
-        int pipe_id = 1;
-        dup2(fd[0*2+READ], STDIN_FILENO); // dup pipe of 1 to in
-        dup2(fd[pipe_id*2+WRITE], STDOUT_FILENO);
-        close(fd[pipe_id*2+READ]);
+        waitpid(pid, &status, 0);
+        waitpid(pid2, &status, 0); 
         close(fd[pipe_id*2+WRITE]);
+        dup2(fd[pipe_id*2+READ], STDIN_FILENO); // dup pipe of 1 to in
+        // dup2(fd[pipe_id*2+WRITE], STDOUT_FILENO);
+        // close(fd[pipe_id*2+READ]);
+        
         execlp("bin/number", "bin/number", (char*) NULL);
         std::cerr << "execute failed" << std::endl;
         exit(1);
     }
-    
+    close(fd[pipe_id*2+WRITE]);
     waitpid(pid3, &status, 0); 
     // waitpid(pid3, &status, WNOHANG);
     std::cout << "parent catch child 3" << std::endl;    
